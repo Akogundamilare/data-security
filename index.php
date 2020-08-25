9272401da83b507844a1d8bc4bf3eaa4c6f1cf9f
Abubakar Sadiq Hassan <abubakarhassan59@gmail.com>
2020-08-25 22:24:35 +0100

    Loaded env package handler

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 require('vendor/autoload.php');
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 require_once './libs/config.php';
 require_once './libs/constants.php';
 ?>
